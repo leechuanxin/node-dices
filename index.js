043288f49bb898ee4fdedcbf6fe9416be43dc3a9
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-05 09:04:24 +0800

    feat: initializing new game.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+import { write } from './jsonFileStorage.js';
+
+// eslint-disable-next-line no-unused-vars
+const handleInit = (writeErr, jsonContentStr) => {
+  if (!writeErr) {
+    console.log('You have started a new game!');
+  }
+};
+
+const init = () => {
+  const obj = {
+    rolls: [],
+    frequencies: {},
+  };
+  write('data.json', obj, handleInit);
+};
+
+if (process.argv[2] === 'init') {
+  init();
+}
