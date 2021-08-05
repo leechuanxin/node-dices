8b8c9ecd0a6c60104adacf2d2a794c8fe6eea025
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-05 09:30:18 +0800

    feat: roll dice and save value.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { write } from './jsonFileStorage.js';
+import * as jsonFileStorage from './jsonFileStorage.js';
+
+// global constants
+const JSON_FILE = 'data.json';
+const ROLLS_KEY = 'rolls';
 
 // eslint-disable-next-line no-unused-vars
 const handleInit = (writeErr, jsonContentStr) => {
@@ -7,14 +11,43 @@ const handleInit = (writeErr, jsonContentStr) => {
   }
 };
 
+// eslint-disable-next-line no-unused-vars
+const handleClear = (writeErr, jsonContentStr) => {
+  if (!writeErr) {
+    console.log('The existing scores have been cleared. You can start a new game by typing in: node index.js init');
+  }
+};
+
+const handleRoll = (consoleStr) => {
+  if (consoleStr === 'Key does not exist') {
+    console.log('Unfortunately, there is no game running now, so your latest roll is not saved.');
+    console.log('Please start a new game by typing in: node index.js init');
+  }
+};
+
 const init = () => {
   const obj = {
     rolls: [],
     frequencies: {},
   };
-  write('data.json', obj, handleInit);
+  jsonFileStorage.write(JSON_FILE, obj, handleInit);
+};
+
+const clear = () => {
+  const obj = {};
+  jsonFileStorage.write(JSON_FILE, obj, handleClear);
+};
+
+const roll = () => {
+  const randomDiceNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(`You rolled a ${randomDiceNumber}.`);
+  jsonFileStorage.add(JSON_FILE, ROLLS_KEY, randomDiceNumber, handleRoll);
 };
 
 if (process.argv[2] === 'init') {
   init();
+} else if (process.argv[2] === 'clear') {
+  clear();
+} else if (process.argv[2] === 'roll') {
+  roll();
 }
