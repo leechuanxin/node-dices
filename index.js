fd12c5c5ee0588db8ea91e29ab490a47f1e6742a
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-05 10:50:10 +0800

    feat: most rolled dice number.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import * as jsonFileStorage from './jsonFileStorage.js';
 
 // global constants
 const JSON_FILE = 'data.json';
-const ROLLS_KEY = 'rolls';
 
 // eslint-disable-next-line no-unused-vars
 const handleInit = (writeErr, jsonContentStr) => {
@@ -18,10 +17,51 @@ const handleClear = (writeErr, jsonContentStr) => {
   }
 };
 
-const handleRoll = (consoleStr) => {
-  if (consoleStr === 'Key does not exist') {
-    console.log('Unfortunately, there is no game running now, so your latest roll is not saved.');
+const handleReadRollFreq = (err, jsonContentObj) => {
+  // Exit if there was an error
+  if (err) {
+    console.error('Edit error', err);
+    return;
+  }
+
+  // Exit if key does not exist in DB
+  if (!('rolls' in jsonContentObj && 'frequencies' in jsonContentObj)) {
+    console.log('Unfortunately, there is no game running now, so you can\'t roll the dice.');
     console.log('Please start a new game by typing in: node index.js init');
+    return;
+  }
+
+  const tally = jsonContentObj.frequencies;
+  const randomDiceNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(`You rolled a ${randomDiceNumber}.`);
+  if (String(randomDiceNumber) in tally) {
+    // If we have seen the dice roll before, increment its count
+    tally[String(randomDiceNumber)] += 1;
+  } else {
+    // Else, initialise count of this dice roll to 1
+    tally[String(randomDiceNumber)] = 1;
+  }
+  jsonContentObj.frequencies = tally;
+
+  // log highest frequency
+  const frequenciesArr = Object.values(jsonContentObj.frequencies);
+  const highestFreq = Math.max(...frequenciesArr);
+  // turn tally object into 2d [roll, rollCount] array
+  const diceRollsHighestFreq = Object.entries(jsonContentObj.frequencies)
+    // retrieve the values that match the highest frequency
+    .filter((roll) => roll[1] === highestFreq)
+    // and only get the diceroll value
+    .map((roll) => roll[0]);
+  console.log(`The computer has rolled ${diceRollsHighestFreq.join(', ')} the most times.`);
+
+  // Add input element to target array
+  jsonContentObj.rolls.push(randomDiceNumber);
+};
+
+const handleWriteRollFreq = (err, jsonContentStr) => {
+  if (!err) {
+    const obj = JSON.parse(jsonContentStr);
+    console.log(`You have rolled the dice ${obj.rolls.length} time(s) so far.`);
   }
 };
 
@@ -47,9 +87,7 @@ const clear = () => {
 };
 
 const roll = () => {
-  const randomDiceNumber = Math.floor(Math.random() * 6) + 1;
-  console.log(`You rolled a ${randomDiceNumber}.`);
-  jsonFileStorage.add(JSON_FILE, ROLLS_KEY, randomDiceNumber, handleRoll);
+  jsonFileStorage.edit(JSON_FILE, handleReadRollFreq, handleWriteRollFreq);
 };
 
 const average = () => {
